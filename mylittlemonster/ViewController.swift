410ee73b8c3361b448f862639be387991c3a124d
rouxbuciu <roux@acmelabs.ca>
2016-07-08 18:25:15 -0400

    Starting project

---
+++ mylittlemonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mylittlemonster
+//
+//  Created by roux g. buciu on 2016-07-08.
+//  Copyright © 2016 ACME Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
