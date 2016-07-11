ca267f29b131ab32b34aba4939abf66abd38c092
rouxbuciu <roux@acmelabs.ca>
2016-07-11 16:56:04 -0400

    Extended UIImageView functionality with DragImg

---
+++ mylittlemonster/DragImg.swift
@@ -0,0 +1,48 @@
+//
+//  DragImg.swift
+//  mylittlemonster
+//
+//  Created by roux g. buciu on 2016-07-11.
+//  Copyright © 2016 ACME Labs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DragImg: UIImageView {
+    
+    var originalPosition: CGPoint!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        originalPosition = self.center
+        
+        
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        if let touch = touches.first {
+        
+            let position = touch.locationInView(self.superview)
+            self.center = CGPointMake(position.x, position.y)
+        
+        }
+        
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       
+        self.center = originalPosition
+        
+    }
+    
+}
--- mylittlemonster/ViewController.swift
+++ mylittlemonster/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var monsterImg: UIImageView!
-    
+    @IBOutlet weak var foodImg: DragImg!
+    @IBOutlet weak var heartImg: DragImg!
     
     
     override func viewDidLoad() {
@@ -33,7 +34,8 @@ class ViewController: UIViewController {
         monsterImg.startAnimating()
     
     }
-
-
+    
+    
+ 
 }
 
