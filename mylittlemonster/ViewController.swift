ebee0999b845f20c09e8f319038b8350cd6d3ec6
rouxbuciu <roux@acmelabs.ca>
2016-07-13 13:24:47 -0400

    Added timer for skull penalty and death animation was activated if timer runs out 3 times

--- mylittlemonster/ViewController.swift
+++ mylittlemonster/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var monsterImg: MonsterImg!
     @IBOutlet weak var foodImg: DragImg!
     @IBOutlet weak var heartImg: DragImg!
+    @IBOutlet weak var skullPenalty1Img: UIImageView!
+    @IBOutlet weak var skullPenalty2Img: UIImageView!
+    @IBOutlet weak var skullPenalty3Img: UIImageView!
+    
+    let DIM_ALPHA: CGFloat = 0.2
+    let OPAQUE: CGFloat = 1.0
+    let MAX_PENALTIES = 3
+    
+    var currentPenalties = 0
+    var timer: NSTimer!
     
     
     override func viewDidLoad() {
@@ -21,7 +31,13 @@ class ViewController: UIViewController {
         foodImg.dropTarget = monsterImg
         heartImg.dropTarget = monsterImg
         
+        skullPenalty1Img.alpha = DIM_ALPHA
+        skullPenalty2Img.alpha = DIM_ALPHA
+        skullPenalty3Img.alpha = DIM_ALPHA
+        
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.itemDroppedOnCharacter(_:)), name: "onTargetDropped", object: nil)
+        
+        startTimer()
     
     }
     
@@ -29,6 +45,48 @@ class ViewController: UIViewController {
         print("Item dropped on character")
     }
     
+    func startTimer() {
+        if timer != nil {
+            timer.invalidate()
+        }
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "changeGameState", userInfo: nil, repeats: true)
+        
+    }
+    
+    func changeGameState() {
+        
+        currentPenalties += 1
+        
+        if currentPenalties == 1 {
+            skullPenalty1Img.alpha = OPAQUE
+            skullPenalty2Img.alpha = DIM_ALPHA
+        
+        } else if currentPenalties == 2 {
+            skullPenalty2Img.alpha = OPAQUE
+            skullPenalty3Img.alpha = DIM_ALPHA
+        
+        } else if currentPenalties <= 3 {
+            skullPenalty3Img.alpha = OPAQUE
+            
+        } else {            // just safety
+            skullPenalty1Img.alpha = DIM_ALPHA
+            skullPenalty2Img.alpha = DIM_ALPHA
+            skullPenalty3Img.alpha = DIM_ALPHA
+            
+        }
+        
+        if currentPenalties >= MAX_PENALTIES {
+            gameOver()
+        }
+    }
+    
+    func gameOver() {
+        
+        timer.invalidate()
+        monsterImg.playDeathAnimation()
+        
+    }
     
  
 }
